6b824b00db3217210d8517b7c01526a83a537a2e
Sameer Faridi <s.faridi007@gmail.com>
2023-12-18 23:11:25 +0530

    socket.io

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
